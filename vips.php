6bc8c28067eff630ae0d6e5143874aca165b89bc
Joao Paulo <joaopaulooz@outlook.com>
2023-01-25 13:12:58 -0300

    atualizacoesmobile

--- vips.php
+++ vips.php
@@ -4,6 +4,7 @@
 <head>
 	<title>Home - Mercearia do Joao</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- responsividade-->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="Icon" href="img/merceariabarra.png">
 </head>
